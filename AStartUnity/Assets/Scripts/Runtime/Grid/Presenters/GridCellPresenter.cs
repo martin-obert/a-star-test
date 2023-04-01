795502494944e4006d2e23a263d8ab254d5877ed
Martin Obert <martin@obert.cz>
2023-04-01 12:47:37 +0200

    feat: GridCell presenter
    - very basic presenter impl.
    - hooked on the grid instantiation logic
    - not much of a setup
    - tightly coupled

---
+++ AStartUnity/Assets/Scripts/Runtime/Grid/Presenters/GridCellPresenter.cs
@@ -0,0 +1,15 @@
+﻿using Runtime.Grid.Data;
+using UnityEngine;
+
+namespace Runtime.Grid.Presenters
+{
+    public sealed class GridCellPresenter : MonoBehaviour
+    {
+        private IGridCell _cell;
+        
+        public void SetDataModel(IGridCell cell)
+        {
+            _cell = cell;
+        }
+    }
+}
--- AStartUnity/Assets/Scripts/Runtime/Grid/Services/GridManager.cs
+++ AStartUnity/Assets/Scripts/Runtime/Grid/Services/GridManager.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using Grid;
 using Runtime.Grid.Data;
+using Runtime.Grid.Presenters;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace Runtime.Grid.Services
 {
@@ -14,9 +16,14 @@ namespace Runtime.Grid.Services
         [SerializeField] private int rowCount = 1;
         [SerializeField] private int colCount = 1;
         [SerializeField] private bool autoGenerateGridOnStart;
+        [SerializeField] private GridCellPresenterSpawner spawner;
 
         private void Awake()
         {
+            Assert.IsNotNull(spawner, "spawner != null");
+            Assert.IsTrue(rowCount > 0, "rowCount > 0");
+            Assert.IsTrue(colCount > 0, "colCount > 0");
+            
             if (Instance != null && !ReferenceEquals(Instance, this))
             {
                 Destroy(this);
@@ -37,6 +44,12 @@ namespace Runtime.Grid.Services
         public void GenerateGrid()
         {
             CurrentCells = GridGenerator.GenerateGrid(rowCount, colCount);
+            
+            foreach (var gridCell in CurrentCells)
+            {
+                // TODO: store the instance for further reuse
+                spawner.SpawnOne(gridCell, transform);
+            }
         }
     }
 }
