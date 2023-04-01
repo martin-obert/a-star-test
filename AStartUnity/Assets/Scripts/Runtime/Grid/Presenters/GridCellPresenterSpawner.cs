45c07542c6d7265214f130ec5acc0e39d6659f55
Martin Obert <30500239+martin-obert@users.noreply.github.com>
2023-04-01 12:53:44 +0200

    feat: GridCell presenter (#5)
    - very basic presenter impl.
    - hooked on the grid instantiation logic
    - not much of a setup
    - tightly coupled

---
+++ AStartUnity/Assets/Scripts/Runtime/Grid/Presenters/GridCellPresenterSpawner.cs
@@ -0,0 +1,18 @@
+﻿using Runtime.Grid.Data;
+using UnityEngine;
+
+namespace Runtime.Grid.Presenters
+{
+    [CreateAssetMenu(menuName = "Grid/Cell Presenter Spawner", fileName = "GridCellPresenterSpawner", order = 0)]
+    public sealed class GridCellPresenterSpawner : ScriptableObject
+    {
+        [SerializeField] private GridCellPresenter presenterPrefab;
+
+        public GridCellPresenter SpawnOne(IGridCell cell, Transform parent)
+        {
+            var instance = Instantiate(presenterPrefab, parent);
+            instance.SetDataModel(cell);
+            return instance;
+        }
+    }
+}
