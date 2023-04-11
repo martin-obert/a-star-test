5e404950d1b4a4cfefe8460fa6e2ed747d97658c
Martin Obert <martin@obert.cz>
2023-04-11 22:17:10 +0200

    feat: added grid cell validations

--- AStartUnity/Assets/Scripts/Runtime/Grid/Presenters/GridCellHelpers.cs
+++ AStartUnity/Assets/Scripts/Runtime/Grid/Presenters/GridCellHelpers.cs
@@ -5,10 +5,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Runtime.Definitions;
 using Runtime.Grid.Data;
+using Runtime.Grid.Services;
+using Runtime.Terrains;
 using UnityEngine;
 
 namespace Runtime.Grid.Presenters
 {
+    public static class ThrowHelpers
+    {
+        public static void ValidateGridCellDataOrThrow(GridCellSave value)
+        {
+            if (value == null) throw new ArgumentNullException(nameof(value));
+            if (value.RowIndex < 0)
+                throw new ArgumentOutOfRangeException(nameof(value.RowIndex), value.RowIndex,
+                    $"{nameof(value.RowIndex)} must be greater or equal to 0");
+            if (value.ColIndex < 0)
+                throw new ArgumentOutOfRangeException(nameof(value.ColIndex), value.ColIndex,
+                    $"{nameof(value.ColIndex)} must be greater or equal to 0");
+            
+            if(value.TerrainType == TerrainType.Unknown)
+                throw new ArgumentOutOfRangeException(nameof(value.TerrainType), value.TerrainType,
+                    "Unsupported terrain type");
+        }
+    }
+    
     public static class GridCellHelpers
     {
         public static bool IsCellOdd(int rowIndex) => rowIndex % 2 != 0;
---
+++ AStartUnity/Assets/Scripts/Tests/DataValidationTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using NUnit.Framework;
+using Runtime.Grid.Presenters;
+using Runtime.Grid.Services;
+using Runtime.Terrains;
+
+namespace Tests
+{
+    public class DataValidationTests
+    {
+        [Test]
+        public void GridCellDataModel_Pass()
+        {
+            var data = new GridCellSave
+            {
+                ColIndex = 0,
+                RowIndex = 0,
+                TerrainType = TerrainType.Grass
+            };
+
+            ThrowHelpers.ValidateGridCellDataOrThrow(data);
+            Assert.Pass();
+        }
+
+        [TestCase(0, -1, TerrainType.Grass)]
+        public void GridCellDataModel_InvalidRowIndex(int colIndex, int rowIndex, TerrainType type)
+        {
+            var data = new GridCellSave
+            {
+                ColIndex = colIndex,
+                RowIndex = rowIndex,
+                TerrainType = type
+            };
+
+
+            Assert.Throws(Is.TypeOf<ArgumentOutOfRangeException>().And.Message.StartsWith("RowIndex"),
+                () => ThrowHelpers.ValidateGridCellDataOrThrow(data));
+        }
+        
+        [TestCase(-1, 0, TerrainType.Grass)]
+        public void GridCellDataModel_InvalidColIndex(int colIndex, int rowIndex, TerrainType type)
+        {
+            var data = new GridCellSave
+            {
+                ColIndex = colIndex,
+                RowIndex = rowIndex,
+                TerrainType = type
+            };
+
+            Assert.Throws(Is.TypeOf<ArgumentOutOfRangeException>().And.Message.StartsWith("ColIndex"),
+                () => ThrowHelpers.ValidateGridCellDataOrThrow(data));
+        }
+        
+        
+        [TestCase(0, 0, TerrainType.Unknown)]
+        public void GridCellDataModel_InvalidTerrainType(int colIndex, int rowIndex, TerrainType type)
+        {
+            var data = new GridCellSave
+            {
+                ColIndex = colIndex,
+                RowIndex = rowIndex,
+                TerrainType = type
+            };
+
+            Assert.Throws(Is.TypeOf<ArgumentOutOfRangeException>().And.Message.Contains("terrain"),
+                () => ThrowHelpers.ValidateGridCellDataOrThrow(data));
+        }
+    }
+}
